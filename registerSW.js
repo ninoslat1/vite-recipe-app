91dbb08e297e4895e0c2647e5405c392e9fcc36a
ninoslat1 <benedict.ellnina@gmail.com>
2024-05-27 14:19:16 +0700

    update gh page

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/vite-recipe-app/sw.js', { scope: '/vite-recipe-app/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
